4e650fe9b8e5c45f18d551505751f2e938423413
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2025-07-22 10:48:40 +0200

    Aggiunto: aggiunto file con le modifiche per la java 17.  Modificato il metodo "XMLUtils.toDOMDocument" levando il parametro "SAML2SDKUtils.debug" perchè non è piu' necessario.

--- src/com/sun/identity/custom/util/NameIDPolicyImplNoAllowCreate.java
+++ src/com/sun/identity/custom/util/NameIDPolicyImplNoAllowCreate.java
@@ -10,47 +10,46 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 public class NameIDPolicyImplNoAllowCreate extends NameIDPolicyImpl implements NameIDPolicy {
-  
-private static final String FORMAT = "Format";
-  
+
+  private static final String FORMAT = "Format";
+
   private static final String SPNAMEQUALIFIER = "SPNameQualifier";
-  
+
   private static final String ALLOWCREATE = "AllowCreate";
-  
+
   private boolean isMutable = false;
-  
+
   private String format;
-  
+
   private String spNameQualifier;
-  
+
   private Boolean allowCreate;
-  
+
   public NameIDPolicyImplNoAllowCreate() {
     this.isMutable = true;
   }
-  
+
   public NameIDPolicyImplNoAllowCreate(Element element) throws SAML2Exception {
     parseElement(element);
   }
 
-    public NameIDPolicyImplNoAllowCreate(String xmlString) throws SAML2Exception {
-        Document xmlDocument = XMLUtils.toDOMDocument(xmlString, SAML2SDKUtils.debug);
-        if (xmlDocument == null)
-            throw new SAML2Exception(SAML2SDKUtils.bundle
-                    .getString("errorObtainingElement"));
-        parseElement(xmlDocument.getDocumentElement());
-    }
-  
+  public NameIDPolicyImplNoAllowCreate(String xmlString) throws SAML2Exception {
+    Document xmlDocument = XMLUtils.toDOMDocument(xmlString);
+    if (xmlDocument == null)
+      throw new SAML2Exception(SAML2SDKUtils.bundle
+          .getString("errorObtainingElement"));
+    parseElement(xmlDocument.getDocumentElement());
+  }
 
   public void removeAllowCreate() {
-	  this.allowCreate = null;
+    this.allowCreate = null;
   }
-  
+
   private void parseElement(Element element) {
-	    this.format = element.getAttribute("Format");
-	    this.spNameQualifier = element.getAttribute("SPNameQualifier");
-	    String allowCreateStr = element.getAttribute("AllowCreate");
-	    if (allowCreateStr != null && allowCreateStr.length() > 0)
-	      this.allowCreate = SAML2SDKUtils.booleanValueOf(allowCreateStr); 
+    this.format = element.getAttribute("Format");
+    this.spNameQualifier = element.getAttribute("SPNameQualifier");
+    String allowCreateStr = element.getAttribute("AllowCreate");
+    if (allowCreateStr != null && allowCreateStr.length() > 0)
+      this.allowCreate = SAML2SDKUtils.booleanValueOf(allowCreateStr);
   }
 }
